f29a67ae8ba70cd1d288443284c9d9f3094a34d4
Sala213 <Sala213@ezse.pl>
2025-09-17 11:22:22 +0200

    przepisane

--- Programowanie/EventVsBindingMauiApp/MainPage.xaml.cs
+++ Programowanie/EventVsBindingMauiApp/MainPage.xaml.cs
@@ -4,6 +4,16 @@
     {
         int count = 0;
 
+        public string Message { get; set; }
+
+        private string returnMessage;
+
+        public string ReturnMessage
+        {
+            get { return returnMessage; }
+            set { returnMessage = value; OnPropertyChanged(); }
+        }
+
         public MainPage()
         {
             InitializeComponent();
@@ -12,6 +22,11 @@
         private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
             rotationLabel.Rotation = rotationSlider.Value;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            ReturnMessage = "ilość to" + Message.Length;
         }
     }
 }
