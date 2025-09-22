01272a5d1f62aed4c9fcbb1adbf9bc0f79afd77b
Sala213 <Sala213@ezse.pl>
2025-09-22 14:36:02 +0200

    zadanie

---
+++ Programowanie/Egzamin2021-06MauiApp/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿namespace Egzamin2021_06MauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Email { get; set; } = "";
+
+        public string Password { get; set; } = "";
+
+        public string PasswordRepeat { get; set; } = "";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private string resoult = "";
+        public string Resoult 
+        {
+            get { return resoult; }
+            set { resoult = value; OnPropertyChanged(); }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (CheckEmail(Email) && CheckPassoword(Password, PasswordRepeat))
+                Resoult = $"Witaj {Email}";
+
+            else if (!CheckEmail(Email))
+                Resoult = "Niepoprawny Email";
+
+            else if (!CheckPassoword(Password, PasswordRepeat))
+                Resoult = "Hasła się różnią";
+        }
+
+        bool CheckEmail(string email)
+        {
+            foreach (var sign in email)
+            {
+                if (sign == '@')
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        bool CheckPassoword(string passoword, string passwordRepeat)
+        {
+            if (passoword == passwordRepeat)
+                return true;
+            else
+                return false;
+        }
+    }
+}
+
+
