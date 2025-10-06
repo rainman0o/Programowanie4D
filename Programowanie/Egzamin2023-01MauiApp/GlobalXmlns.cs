f6d1e8512d7d60600097e83e7782eca03b6a529c
Sala213 <Sala213@ezse.pl>
2025-10-06 15:25:06 +0200

    xa

--- Programowanie/Egzamin2023-01MauiApp/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Egzamin2023_01MauiApp")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Egzamin2023_01MauiApp.Pages")]
