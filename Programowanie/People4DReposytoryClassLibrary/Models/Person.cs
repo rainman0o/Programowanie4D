9aca27695c5083e5e17b9f22f0a6e1973637ccdc
Sala213 <Sala213@ezse.pl>
2026-03-02 15:15:59 +0100

    bazy danych

---
+++ Programowanie/People4DReposytoryClassLibrary/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace People4DReposytoryClassLibrary.Models;
+
+public partial class Person
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Surname { get; set; } = null!;
+
+    public int Age { get; set; }
+}
---
+++ Programowanie/People4DReposytoryClassLibrary/PeopleDBContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using People4DReposytoryClassLibrary.Models;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace People4DReposytoryClassLibrary;
+
+public partial class PeopleDBContext : DbContext
+{
+    public PeopleDBContext()
+    {
+    }
+
+    public PeopleDBContext(DbContextOptions<PeopleDBContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=localhost;database=people4d2026;user=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.4.32-mariadb"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_general_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Person>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("person");
+
+            entity.Property(e => e.Id).HasColumnType("int(11)");
+            entity.Property(e => e.Age).HasColumnType("int(11)");
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Surname).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
